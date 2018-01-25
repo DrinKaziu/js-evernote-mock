81b0267a29d72530d95004ecbea27635dbe52136
perpepajn25 <perpepajn25@gmail.com>
2018-01-25 13:06:15 -0500

    remove content from index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const noteList = document.getElementById('note-list')
 
-  fetch('http://localhost:3000/api/v1/notes')
-    .then(res => res.json())
-    .then(json => {
-      json.forEach(note => {
-        let noteDiv = document.createElement('div')
-        noteDiv.innerHTML = `
-          <h3> ${note.title} </h3>
-          <p> ${note.body} </p>
-        `
-        noteDiv.className = 'note-card'
-        // noteDiv.addEventListener('click', function(event){showNotePreview(note)})
-        noteList.appendChild(noteDiv)
-      })
-    })
-})
